362cfcf6c81f82aea916e9ba9dfc58c521cc3b3f
nathanmartinszup <nathan.martins@zup.com.br>
2021-04-19 15:25:39 -0300

    Adding repository tests

---
+++ auth/internal/repositories/account/account_test.go
@@ -0,0 +1,76 @@
+package account
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ZupIT/horusec-devkit/pkg/services/database"
+	"github.com/ZupIT/horusec-devkit/pkg/services/database/response"
+
+	accountEntities "github.com/ZupIT/horusec-platform/auth/internal/entities/account"
+	accountUseCases "github.com/ZupIT/horusec-platform/auth/internal/usecases/account"
+)
+
+func TestNewAccountRepository(t *testing.T) {
+	t.Run("should create account repository", func(t *testing.T) {
+		assert.NotNil(t, NewAccountRepository(&database.Connection{}, nil))
+	})
+}
+
+func TestGetAccount(t *testing.T) {
+	t.Run("should success get a account by id", func(t *testing.T) {
+		databaseMock := &database.Mock{}
+		databaseMock.On("Find").Return(&response.Response{})
+
+		repository := NewAccountRepository(&database.Connection{Read: databaseMock, Write: databaseMock},
+			accountUseCases.NewAccountUseCases())
+
+		account, err := repository.GetAccount(uuid.New())
+		assert.NoError(t, err)
+		assert.NotNil(t, account)
+	})
+}
+
+func TestGetAccountByEmail(t *testing.T) {
+	t.Run("should success get a account by email", func(t *testing.T) {
+		databaseMock := &database.Mock{}
+		databaseMock.On("Find").Return(&response.Response{})
+
+		repository := NewAccountRepository(&database.Connection{Read: databaseMock, Write: databaseMock},
+			accountUseCases.NewAccountUseCases())
+
+		account, err := repository.GetAccountByEmail("test@test.com")
+		assert.NoError(t, err)
+		assert.NotNil(t, account)
+	})
+}
+
+func TestGetAccountByUsername(t *testing.T) {
+	t.Run("should success get a account by username", func(t *testing.T) {
+		databaseMock := &database.Mock{}
+		databaseMock.On("Find").Return(&response.Response{})
+
+		repository := NewAccountRepository(&database.Connection{Read: databaseMock, Write: databaseMock},
+			accountUseCases.NewAccountUseCases())
+
+		account, err := repository.GetAccountByUsername("test")
+		assert.NoError(t, err)
+		assert.NotNil(t, account)
+	})
+}
+
+func TestCreateAccount(t *testing.T) {
+	t.Run("should success create account without errors", func(t *testing.T) {
+		databaseMock := &database.Mock{}
+		databaseMock.On("Create").Return(&response.Response{})
+
+		repository := NewAccountRepository(&database.Connection{Read: databaseMock, Write: databaseMock},
+			accountUseCases.NewAccountUseCases())
+
+		account, err := repository.CreateAccount(&accountEntities.Account{})
+		assert.NoError(t, err)
+		assert.NotNil(t, account)
+	})
+}
