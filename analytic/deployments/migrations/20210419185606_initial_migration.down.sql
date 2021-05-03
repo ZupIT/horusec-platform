d520c8c990d4e98d6387b6e874ec06e017979ca8
Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>
2021-05-03 09:02:47 -0300

    Removinb analytic migration
    delete the analytic migration path to use devkit instead

--- analytic/deployments/migrations/20210419185606_initial_migration.down.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
-DROP TABLE IF EXISTS vulnerabilities_by_author CASCADE;
-DROP TABLE IF EXISTS vulnerabilities_by_language CASCADE;
-DROP TABLE IF EXISTS vulnerabilities_by_repository CASCADE;
-DROP TABLE IF EXISTS vulnerabilities_by_time CASCADE;
-
-COMMIT;
