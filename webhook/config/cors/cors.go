3cedf5dd183e8d5d8630e84fb9053ca5ec11c69b
nathanmartinszup <nathan.martins@zup.com.br>
2021-05-07 10:56:25 -0300

    Updating webhook cors

--- webhook/config/cors/cors.go
+++ webhook/config/cors/cors.go
@@ -5,7 +5,7 @@ import "github.com/go-chi/cors"
 func NewCorsConfig() *cors.Options {
 	return &cors.Options{
 		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "headers", "X-Horusec-Authorization", "Content-Type"},
 		AllowCredentials: true,
 		MaxAge:           300,
