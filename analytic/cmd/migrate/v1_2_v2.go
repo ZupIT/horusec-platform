fdd20af94e73c4c3d37f5c5c95319b4d58567c5e
Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>
2021-05-07 21:23:35 -0300

    Adding migrate broker publisher

--- analytic/cmd/migrate/v1_2_v2.go
+++ analytic/cmd/migrate/v1_2_v2.go
@@ -5,21 +5,28 @@ import (
 	"fmt"
 
 	"github.com/ZupIT/horusec-devkit/pkg/entities/analysis"
+	"github.com/ZupIT/horusec-devkit/pkg/enums/exchange"
+	"github.com/ZupIT/horusec-devkit/pkg/services/broker"
+	brokerconfig "github.com/ZupIT/horusec-devkit/pkg/services/broker/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// brokerConfig := brokerconfig.NewBrokerConfig()
-	// broker, err := broker.NewBroker(brokerConfig)
+	brokerConfig := brokerconfig.NewBrokerConfig()
+	broker, _ := broker.NewBroker(brokerConfig)
 
 	databaseURI := flag.String("v1-database-uri", "", "")
 	flag.Parse()
 
 	conn, _ := gorm.Open(postgres.Open(string(*databaseURI)), &gorm.Config{})
 
-	analysis := &analysis.Analysis{}
+	analysis := &[]analysis.Analysis{}
 	conn.Table("analysis").Preload("AnalysisVulnerabilities").Find(analysis)
 
-	fmt.Printf(`%+v\n`, analysis)
+	for _, analyse := range *analysis {
+		broker.Publish("", exchange.NewAnalysis.ToString(), exchange.Fanout.ToString(), analyse.ToBytes())
+		fmt.Printf(`%+v\n`, analyse)
+	}
+
 }
