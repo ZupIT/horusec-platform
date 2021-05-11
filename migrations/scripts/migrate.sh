fcc1c41d633b7b557eb4c6638ef27120bccf51a3
Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>
2021-05-11 14:21:03 -0300

    Fixing migragte command

--- migrations/scripts/migrate.sh
+++ migrations/scripts/migrate.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-migrate -path "/horusec-migrations/$MIGRATION_NAME" -database "$HORUSEC_DATABASE_SQL_URI" "$@"
+migrate -path "/horusec-migrations/$MIGRATION_NAME" -database "$HORUSEC_DATABASE_SQL_URI" up
