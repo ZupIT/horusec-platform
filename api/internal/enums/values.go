eb05b69949287a9c17b01581469a6a9e72f24f57
nathanmartinszup <nathan.martins@zup.com.br>
2021-07-01 14:59:04 -0300

    Fixing error in ldap groups and heritage

--- api/internal/enums/values.go
+++ api/internal/enums/values.go
@@ -1,8 +1,10 @@
 package enums
 
 const (
-	DefaultPort    = "8000"
-	BaseRouter     = "/api"
-	HealthRouter   = BaseRouter + "/health"
-	AnalysisRouter = BaseRouter + "/analysis"
+	DefaultPort             = "8000"
+	BaseRouter              = "/api"
+	HealthRouter            = BaseRouter + "/health"
+	AnalysisRouter          = BaseRouter + "/analysis"
+	DatabaseRepositoryTable = "repositories"
+	DatabaseWorkspaceTable  = "workspaces"
 )
